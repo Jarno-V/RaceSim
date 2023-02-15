69b8a7657bff84390e49a69d24a0c90ef0742a0f
Jarno-V <54915026+Jarno-V@users.noreply.github.com>
2023-02-15 11:48:24 +0100

    level 2 classes maken

--- raceSimulator/Model/Car.cs
+++ raceSimulator/Model/Car.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    internal class Car
+    internal class Car : IEquipment
     {
-
+        public int Quility { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Performance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool isBroken { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
