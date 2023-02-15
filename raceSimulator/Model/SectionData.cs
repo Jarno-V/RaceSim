2566a493204ec75f09b91f590013a1e6b1489d6e
Jarno-V <54915026+Jarno-V@users.noreply.github.com>
2023-02-15 13:15:36 +0100

    implements tussen classes en interfaces gefixed

--- raceSimulator/Model/SectionData.cs
+++ raceSimulator/Model/SectionData.cs
@@ -6,19 +6,13 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    internal class SectionData : IParticipant
+    internal class SectionData
     {
         IParticipant left { get; set; }
         IParticipant right { get; set; }
         int DistanceLeft { get; set; }
         int DistanceRight { get; set; }
 
-
-
-        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int Points { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public IEquipment Equipment { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public TeamColors TeamColor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
     }
 }
